0369f0d0e2076cac3758d285f1aa51174177c2ff
Gdreyes172 <reyesgr11@gmail.com>
2022-05-01 23:21:26 -0500

    Update

--- PyPoll.py
+++ PyPoll.py
@@ -1,22 +1,112 @@
-# The data we need to retrieve
-# 1) Get the total# of votes
-# 2) Get a complete list of Candidates
-# 3) Get the % of votes per Candidate
-# 4) The total# of votes per Candidate
-# 5) The winner of the election based on popular vote
-
-# Open the data file
 import csv
 import os
+
+# The data we need to retrieve.
+# 1. The total number of votes cast.
+# 2. A complete list of candidates who received votes.
+# 3. The percentage of votes each candidate won.
+# 4. The total number of votes each candidate won.
+# 5. The winner of the election based on popular vote.
+
 # Assign a variable for the file to load and the path.
-file_to_load = os.path.join("C:/Users/Gustavo/OneDrive/Documents/0001_DataAnalystCourse/03_Module3/Election_Analysis/Resources/election_results.csv")
+file_to_load = os.path.join("resources", "election_results.csv")
+
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Initialize vote counter.
+total_votes = 0
+
+# Initialize list of candidate names.
+candidate_options = []
+
+# Initialize dictionary for tabulation of candidate votes.
+candidate_votes = {}
+
+# Winning candidate and winning counter tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
 
-    # Print the file object.
-     print(election_data)
+    # To do: read and analyze the data here.
+    file_reader = csv.reader(election_data)
 
-# Create a filename variable to a direct to indirect path to the file
-file_to_save = os.path.join("C:/Users/Gustavo/OneDrive/Documents/0001_DataAnalystCourse/03_Module3/Election_Analysis/analysis",'election_analysis.txt')
-# Using the open() function with the "w" mode we will write data to the file.
-open(file_to_save, "w")
+    # Read header row.
+    headers = next(file_reader)
+
+    # Print each row in the source file.
+    for row in file_reader:
+
+        # Add to the total vote count.
+        total_votes += 1
+
+        # Store candidate name.
+        candidate_name = row[2]
+
+        # If the candidate does not match any existing candidate...
+        if candidate_name not in candidate_options:
+
+            # Add candidate name to the candidate list.
+            candidate_options.append(candidate_name)
+            
+            # Track candidate's vote coin.
+            candidate_votes[candidate_name] = 0 
+
+        # Add a vote to a candidate's total.
+        candidate_votes[candidate_name] += 1
+
+# Save results to text file
+with open(file_to_save, "w") as txt_file:
+
+# Print the final vote count
+    election_results = (
+        f"\nElection Results\n"
+        f"-------------------------\n"
+        f"Total Votes: {total_votes:,}\n"
+        f"-------------------------\n")
+    print(election_results, end="")
+    # Save the final vote count to the text file.
+    txt_file.write(election_results)
+
+    # Determine the percentage of votes for each candidate by looping through the counts.
+    # Iterate through the candidate list.
+    for candidate_name in candidate_options:
+        # Retrieve vote count of a candidate
+        votes = candidate_votes[candidate_name]
+        # Calculate the percentage
+        vote_percentage = float(votes) / float(total_votes) * 100
+
+        # Print out each candidate's name, vote count, and percentage of votes
+        candidate_results = (f"{candidate_name}: {vote_percentage:.1f}% ({votes:,}) \n")
+
+        # Print eachj candidate, their voter count, and percentage
+        print(candidate_results)
+
+        # Save the candidate results to text file
+        txt_file.write(candidate_results)
+
+        # Determin winning vote count and candidate
+        # Determine if the votes is greater than the winning count.
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            # If true then set winning_count = votes and winning_percent = vote_percentage
+            winning_count = votes
+            winning_percentage = vote_percentage
+            # Set winning_candidate equal to the candidate's name
+            winning_candidate = candidate_name
+
+    winning_candidate_summary = (
+        f"---------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"---------------------------\n")
+    print(winning_candidate_summary)
+    
+    # Save the winning candidate's name to the text file
+    txt_file.write(winning_candidate_summary)
+
+    # Close the file.
+    election_data.close()
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Election_Analysis
-Using Python to perform an election analysis
+# Election Analysis
+# Overview of Election Audit
+## The purpose of this analysis is to determine the winner using python language automate the election results. The code will provide a summary of the three candiates vote pecentage, the largest county voter turnout and a summary of each county voter turnout, and the winner's outcome. 
+# Results:
+Image
+### Final Results.
+## The Winner with about 74% of the overall vote count was Diana DeGette. Her overall votes were 272,892 out of 369,711.
+# 
+#
+## Based on the results Jefferson and Arapahoe county did not have even half of their voter turnout. It can be assumed that most of the candidates efforts were focused solely on Denver county. Simple cross multiplication can give a glimpse of how many votes were left over in Jefferson county and Arapahoe county. Jefferson came in with about 10% of their votes at 38,855. Simple algebra would provide us with the other 90%, whic is about 388,000 voters who didn't participate in the election. Arapahoe county had 93.3% of their voter not show up to the polls which appear to be about 345,000 votes that were missed out on. 
+---
+# Summary:
+## Unsure of the reason for only focusing on Denver county, but there was a clear gap in voter turnout in Jefferson and Arapahoe county. Either of the other two candidates could have stole the race, or at least made it less of a land-slide victory by appealing to voters in other counties. That said, Diana DeGette did really well to have secured her victory.
